9c4e453fa80ed852cc93959b555b8d57ca491f1f
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-05-27 07:33:55 +0530

    To debug wheel angle

--- uruhl_test.py
+++ uruhl_test.py
@@ -1,14 +1,16 @@
 import gym
 import time
+import math
 env = gym.make('Uruhl-v0')
-a = [-10,0]
-b = [-10,0]
+a = [-90,90]
+b = [90,-90]
 action = a
-for i_episode in range(1):
+for i_episode in range(100):
     observation = env.reset()
-    for t in range(1000):
+    for t in range(3000):
         env.render()
-        if (1 and t %100==0):
+        action = [10, -10]#t/30]
+        if (0 and t %100==0):
             if (action == a):
                 action = b
             else:
@@ -23,9 +25,9 @@ for i_episode in range(1):
 	if (0 and t>300):
 	    action = [0,0]
         observation, reward, done, info = env.step(action)
-	time.sleep(0.05) 
+	time.sleep(0.09)
 	#print(reward,done)
-	#print observation
+	print "Angle",((observation[6])*(180/math.pi))%180
         if done:
             print("Episode {} finished after {} episodes".format(i_episode,t+1))
             time.sleep(1.2)
