b106db383e340f52a8c27f32de72f1090a2a1b1f
Akshay Sreekumar <akshaysreekumar1997@gmail.com>
2018-06-08 00:17:36 +0530

    Angle PERFECTLY working
    Balancing perfectly with high pull up factor

--- real_uruhl.py
+++ real_uruhl.py
@@ -9,15 +9,15 @@ from matplotlib import cm
 
 #### Learning related constants ####
 MIN_EXPLORE_RATE = 0.01 #The min exploration rate; The max is 1
-PULL_UP_EXPLORE_LINE = 3 #Increase this to decrease the rate of decrease of epsilon
+PULL_UP_EXPLORE_LINE = 10 #Increase this to decrease the rate of decrease of epsilon
 
 START_LEARNING_RATE = 1 #The max learning_rate ITS FOR Q FUNCTION AND NOT GRADIENT DESCENT...
 MIN_LEARNING_RATE = 0.01   #The min learning_rate
-PULL_UP_LEARN_RATE = 3
+PULL_UP_LEARN_RATE = 10
 
 START_DISCOUNT_FACTOR = 0 #The min discount_factor
 MAX_DISCOUNT_FACTOR = 0.99  #The max discount_factor
-PULL_UP_DISC_FACTOR = 3
+PULL_UP_DISC_FACTOR = 10
 
 #    exp_qVal = (1-learning_rate)* curQval  + learning_rate*( reward + discount_factor*nextMaxQval )
 
@@ -26,30 +26,27 @@ TF_LEARN_RATE = 0.005 #Learning Rate for Gradient Descent
 #### Defining the simulation related constants ####
 
 #Defines the number of episodes it should perform the increment/decrement of values
-NUM_EPISODES = 2000
-NUM_EPISODES_PLATEAU_EXPLORE =  3000*NUM_EPISODES/5000
-NUM_EPISODES_PLATEAU_LEARNING = 2000*NUM_EPISODES/5000
-NUM_EPISODES_PLATEAU_DISCOUNT = 2000*NUM_EPISODES/5000
+NUM_EPISODES = 5000
+NUM_EPISODES_PLATEAU_EXPLORE =  3000*5/5
+NUM_EPISODES_PLATEAU_LEARNING = 2000*5/5
+NUM_EPISODES_PLATEAU_DISCOUNT = 2000*5/5
 
 STREAK_TO_END = 120
-SOLVED_T = 100          # anything more than this returns Done = true for the openAI Gym
-IMU_sensor_count = 6
+SOLVED_T = 500          # anything more than this returns Done = true for the openAI Gym
+
 NEG_REW = -50 #negative reward for fallen pole
 DISPLAY_RATES = True#False #To display the rates as a graph over time
 DISPLAY_ENV = True  #To display the render for enviroment
-if DISPLAY_ENV ==True:
-    from time import sleep
+# if DISPLAY_ENV ==True:
+#     from time import sleep
 
 # number of neurons in each layer
-input_num_units = 8
-hidden_num_units1 = 30
-hidden_num_units2 = 30
-hidden_num_units3 = 30
+input_num_units = 3
+hidden_num_units1 = 20
+hidden_num_units2 = 20
+hidden_num_units3 = 20
 output_num_units = 1
-seed = 0
-
-#def pcom(s):
-    #print(s, end='', flush=True)
+seed = 10
 
 def get_explore_rate(t):
     maxValReached = math.log10(NUM_EPISODES_PLATEAU_EXPLORE)
@@ -87,8 +84,6 @@ if DISPLAY_RATES:
 
 # define placeholders
 tf_x = tf.placeholder(tf.float32, [None, input_num_units],name="Input")
-#y = tf.placeholder(tf.float32, [1, output_num_units],name="Output")
-# tf_qval = tf.placeholder(tf.float32,[1,1],name="Q_value")
 tf_exp_q =  tf.placeholder(tf.float32,[None,1],name="Expected_Q_value")
 
 if 0:
@@ -123,20 +118,21 @@ train_op = optimizer.minimize(cost)
 env = gym.make('Uruhl-v0')
 cost_plot = []
 reward_plot = []
-observation = np.ndarray(shape=(IMU_sensor_count+0,1))
+#observation = np.ndarray(shape=(8,1))
+IM_INTERVAL = 100
 with tf.Session() as sess:
     # create initialized variables
     sess.run(tf.global_variables_initializer())
     ep = 0
-    observa = env.reset()
-    observation = observa[0:IMU_sensor_count]
     while ep<=NUM_EPISODES:
         explore_rate = get_explore_rate(ep)
         learning_rate = get_learning_rate(ep)
         discount_factor = get_discount_factor(ep)
-        # observa = env.reset()
-        # np.copyto(observation,observa)
-        # np.put(observation,[6,7],[((observa[6])*(180/math.pi))%180,((observa[7])*(180/math.pi))%180])
+        observation = env.reset()
+        #np.copyto(observation,observa)
+        #np.put(observation,[6,7],[((observa[6])*(180/math.pi))%180,((observa[7])*(180/math.pi))%180])
+        #if (DISPLAY_ENV == True and ep > (NUM_EPISODES-200)):
+        #    env.render()
         tot_cost = 0
         tot_rew = 0
 
@@ -145,7 +141,6 @@ with tf.Session() as sess:
     # if max=True, return the (maxQ, bestAction)
     # if max = False, return the (bestQ, correspondingAction) based on explore_rate
         def Q(observation,max):
-            #array returned, make scalar
             action_scaling = 5
             act_num = 10
             acto = np.zeros(2*act_num+1)
@@ -157,39 +152,40 @@ with tf.Session() as sess:
             maxQ = acto[act]
             act = act-act_num
             maxA = [act*action_scaling,-act*action_scaling]
-            # if t>2:
-            #     plt.rcdefaults()
-            #     plt.bar(acto,np.arange(-5,5),align='center',alpha=0.5)
-            #     plt.yticks(acto, np.arange(-5,5))
-            #     plt.xlabel('Action')
-            #     plt.title('Q value for action')
-            #     plt.show()
+            if t==10 and ep%IM_INTERVAL == 0:
+                plt.rcdefaults()
+                plt.bar(np.arange(-act_num,act_num+1,1),np.concatenate(acto))#,align='center',alpha=1)
+                #plt.yticks(acto, np.arange(-5,5))
+                plt.xlabel('Action')
+                x = 'Angle = '+str(observation)+ 'with Angular Velocity ='+str(av)
+                plt.title(x)
+                x = 'fig'+str(int(ep/IM_INTERVAL))+'.png'
+                plt.savefig('./bargraph_angle/'+x)
+                plt.clf()
 
             if (max ==True):
                 return (maxQ, maxA)
             else:
                 if(random.random()<explore_rate): # EXPLORE high explore rate => more exploration
-                    act = random.randrange(2*act_num+1)-act_num
-                    #print "RANDOM"
-                    return(acto[act+act_num],[act*action_scaling,-act*action_scaling])
+                    act = random.randrange(21)-10
+                    return(acto[act+10],[act*5,-act*5])
                 else:                             # DONT EXPLORE
-                    #print "not random"
                     return (maxQ, maxA)
+
 ################################################################################################
         for t in range(SOLVED_T):
             pobs = observation
             curQval,action = Q(pobs,False)
-            observa,reward,done,_ = env.step(action)
-            observation = observa[0:IMU_sensor_count]
-            # np.copyto(observation,observa)
-            # np.put(observation,[6,7],[((observa[6])*(180/math.pi))%180,((observa[7])*(180/math.pi))%180])
-            if (DISPLAY_ENV == True):# and ep > (NUM_EPISODES-2000):
+            observation,reward,done,av = env.step(action)
+            #np.copyto(observation,observa)
+            #np.put(observation,[6,7],[((observa[6])*(180/math.pi))%180,((observa[7])*(180/math.pi))%180])
+            if (DISPLAY_ENV == True and ep > (NUM_EPISODES-200)):
                 env.render()
             nextMaxQval,_ = Q(observation, True)
-            if done == True :#and tot_rew<10000:
-                 reward = NEG_REW
+            if done == True and tot_rew<SOLVED_T:
+                reward = NEG_REW
             else:
-                 reward = 1
+                reward = 1
             exp_qVal = (1-learning_rate)* curQval  + learning_rate*( reward + discount_factor*nextMaxQval )
             action_array = np.asarray(action).reshape([1,2])
             exp_qVal_array = np.asarray(exp_qVal).reshape([1,1])
@@ -202,9 +198,9 @@ with tf.Session() as sess:
                 Z = np.vstack([Z,exp_qVal_array])
             tot_rew +=reward
             if done == True:
-                observa = env.reset()
-                observation = observa[0:IMU_sensor_count]
+                break
         _,c = sess.run([train_op,cost], {tf_x: I, tf_exp_q: Z})
+
         if(ep%1 == 0):
             cost_plot = np.append(cost_plot,c)#tot_cost)
             reward_plot = np.append(reward_plot, tot_rew)
@@ -219,8 +215,8 @@ with tf.Session() as sess:
     f, axarr = plt.subplots(2, sharex=True)
     axarr[0].plot(cost_plot)
     axarr[0].set_title('cost_plot')
-    axarr[0].set_ylim([0, 1])
+    #axarr[0].set_ylim([0, 1])
     axarr[1].plot(reward_plot)
     axarr[1].set_title('reward_plot')
-    plt.savefig('./cost.png')
+    plt.savefig('./bargraph_angle/cost_angle.png')
     #plt.show()
